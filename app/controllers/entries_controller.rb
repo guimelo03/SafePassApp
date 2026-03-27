327a271c101b6fb418ee58f73b227cb67a5b6d9a
guimelo03 <guilhermemelo03@hotmail.com>
2026-03-27 15:14:56 -0300

    feat: edit button on our index

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_entry, only: %i[show destroy]
+  before_action :set_entry, only: %i[show edit destroy]
 
   def index
     @entries = current_user.entries
@@ -25,6 +25,12 @@ class EntriesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   def destroy
