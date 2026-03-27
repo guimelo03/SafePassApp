2be19b02b92a3bac88a2334bb764aef22669c01d
guimelo03 <guilhermemelo03@hotmail.com>
2026-03-27 15:42:14 -0300

    feat: update method and turbo_stream

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,9 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_entry, only: %i[show edit destroy]
+  before_action :set_entry, only: %i[show edit update destroy]
 
   def index
-    @entries = current_user.entries
+    @entries = current_user.entries.order(:name)
     @main_entry = current_user.entries.first
   end
 
@@ -31,6 +31,15 @@ class EntriesController < ApplicationController
   end
 
   def update
+    if @entry.update(entry_params)
+      flash.now[:notice] = "#{@entry.name} has been updated."
+      respond_to do |format|
+        format.html { redirect_to @entry }
+        format.turbo_stream { }
+      end
+    else
+      render :edit, status: :unprocessabel_entity
+    end
   end
 
   def destroy
